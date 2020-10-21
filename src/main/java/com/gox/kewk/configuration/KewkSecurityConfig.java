4f9f2567ef9c81de8541f0da45c9c950afcf9538
GoX1337 <gox.roche@gmail.com>
2020-10-21 14:58:26 +0200

    Permit /actuator routes

--- src/main/java/com/gox/kewk/configuration/KewkSecurityConfig.java
+++ src/main/java/com/gox/kewk/configuration/KewkSecurityConfig.java
@@ -14,6 +14,7 @@ public class KewkSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                     .antMatchers("/kewk").permitAll()
+                    .antMatchers("/actuator/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
