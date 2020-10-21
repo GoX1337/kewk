7f3a6bcdc5159b11bb4530e84f99e56a49242d9a
GoX1337 <gox.roche@gmail.com>
2020-10-21 15:15:49 +0200

    Error controller

---
+++ src/main/java/com/gox/kewk/controller/KewkErrorController.java
@@ -0,0 +1,21 @@
+package com.gox.kewk.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KewkErrorController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Forbidden";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
